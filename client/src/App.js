f8fca03d5e4b6aaeb77c1e3e0185c28c6c853531
Anthony Dcosta <adcosta2000@gmail.com>
2022-08-30 21:16:06 -0400

    added apollo client to the App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client';
+
+const httpLink = createhHttplink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <header className="App-header">
+          
+        </header>
+      </div>
+    </ApolloProvider>
+    
   );
 }
 
