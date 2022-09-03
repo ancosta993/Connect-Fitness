4dc427548f56940b6b44baf829f6ddfaca6ec675
seanxmcdaniel <102200863+seanxmcdaniel@users.noreply.github.com>
2022-09-03 19:40:01 -0400

    routine form

--- client/src/component/pages/Routine.js
+++ client/src/component/pages/Routine.js
@@ -1,22 +1,36 @@
 import React, { useState } from 'react';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box'
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
 
 const Routine = () => {
 
     return (
-        
+
         <>
-         <Box component='div' sx={{pt: "2em", pb:"2em", backgroundColor:'white'}}>
-            <Grid container spacing={3}>
-               <Grid item xs={12} md={9}>
-               </Grid>
-               <Grid item xs={12} md={3}>
-                  <Box component='div' style={{background: 'gray'}}>
-                  </Box>
-               </Grid>
-            </Grid>
-         </Box>
+            <Box component='div' sx={{ pt: "5em", pb: "30em", backgroundColor: 'white' }}>
+                <Grid container>
+                    <Grid item xs={12}>
+                        <Box component="form"
+                            sx={{
+                                '& .MuiTextField-root': { m: 1, width: '30ch' },
+                            }}
+                            noValidate
+                            autoComplete="off"
+                            border='1px solid black'>
+                            <div>
+                                <TextField
+                                    required
+                                    id="outlined-required"
+                                    label="Title"
+                                    placeholder="Cardio"
+                                />
+                            </div>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Box>
         </>
 
     )
