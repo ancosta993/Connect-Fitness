bd1661f4a2dfb6eb1caae7064db5fb0833f496f7
Anthony Dcosta <adcosta2000@gmail.com>
2022-09-04 00:23:18 -0400

    added form validation for loginPage

--- client/src/component/pages/LoginPage.js
+++ client/src/component/pages/LoginPage.js
@@ -1,17 +1,40 @@
 import React, {useState} from 'react';
 import Button from '@mui/material/Button';
 import TextField  from '@mui/material/TextField';
+
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogActions from '@mui/material/DialogActions';
+
 import Auth from '../../utils/auth';
 import {useMutation} from '@apollo/client';
 import {LOGIN_USER} from '../../utils/mutations'
 
+
 const LoginPage = () => {
    const [login, { error }] = useMutation(LOGIN_USER);
    const [formData, setFormData] = useState({email:'', password: ''});
+
+   //for dialog start
+   const [open, setOpen] =useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  // for dialo ends
+
+   // declare all the errors
+   const [emailErr, setEmailErr] = useState(false);
+   const [passErr, setPassErr] = useState(false);
+
+   
    // use this function to link the state with the form data.
    const handleChange = (event) => {
-      const { name, value } = event.target;
-  
+      const { name, value } = event.target;  
       setFormData({
         ...formData,
         [name]: value,
@@ -21,31 +44,46 @@ const LoginPage = () => {
    const handleSubmit = async (e) => {
       e.preventDefault();
       // handle error with try/catch method
-      try {
+      setEmailErr(false);
+      setPassErr(false);
+
+      if(formData.email === ''){
+         setEmailErr(true);
+      };
+
+      if(formData.password === ''){
+         setPassErr(true);
+      }
+      
+      if (formData.email && formData.password){
+         try {
          const {data} = await login({variables: {...formData}})
          Auth.login(data.login.token);
-      } catch(e) {
-         console.error(e);
+         } catch(e) {
+            console.error(e);
+         }
+         // reset the form by clearing value
+         setFormData({
+            email: '',
+            password: ''
+         })
+      } else {
+         handleClickOpen()
       }
-
-      // reset the form by clearing value
-      setFormData({
-         email: '',
-         password: ''
-      })
+      
    };
   
    return(
       <section className='signup-container'>
          <div className='signup-placeholder'>Text</div>
-         <form className='signup-form' onSubmit={handleSubmit}>
+         <form noValidate className='signup-form' onSubmit={handleSubmit}>
             <h1 className='signup-title'>Let's Start!</h1>
             <div>
-               <TextField sx={{width:'35ch'}} name='email' label='email' variant='outlined' onChange={handleChange}  value={formData.email} required/>
+               <TextField error={emailErr} sx={{width:'35ch'}} name='email' label='email' variant='outlined' onChange={handleChange}  value={formData.email} required/>
             </div>
 
             <div>
-               <TextField sx={{width:'35ch'}} name='password' label='password' variant='outlined' onChange={handleChange} value={formData.password} required />
+               <TextField error={passErr} sx={{width:'35ch'}} name='password' label='password' variant='outlined' onChange={handleChange} value={formData.password} required />
             </div>
             
             <div>
@@ -54,6 +92,15 @@ const LoginPage = () => {
                </Button>
             </div>
          </form>
+         <Dialog 
+            open={open}
+            onClose={handleClose}
+         >
+            <DialogTitle>{"You must enter all the highlighted fields!"}</DialogTitle>
+            <DialogActions sx={{display:'flex',justifyContent:'center'}}>
+               <Button onClick={handleClose}>Close</Button>
+            </DialogActions>
+         </Dialog>
       </section>
       
    )
